6f53277c3f5e87687c629a6c75f52a7b1fc6c849
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-08 21:23:59 +0200

    dev created user schemas

---
+++ apps/authorization/schemas.py
@@ -0,0 +1,30 @@
+"""Authorization schemas."""
+from datetime import datetime
+
+from apps.common.schemas import BaseInSchema, BaseOutSchema
+
+
+class TokenPayload(BaseInSchema):
+    """Token payload schema."""
+
+    exp: int
+    sub: str
+
+
+class UserOut(BaseOutSchema):
+    """User out schema."""
+
+    id: int
+    username: str
+    email: str
+    is_active: bool = False
+    is_admin: bool = False
+    last_visit_at: datetime
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserIn(TokenPayload):
+    """User in schema."""
+
+    id: int
