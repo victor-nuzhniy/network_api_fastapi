55ff0cbcb1a0c2bc477dff10684fe0fbbfe705b6
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-10 21:53:21 +0200

    dev changed EMAIL_REGEX constant

--- apps/common/constants.py
+++ apps/common/constants.py
@@ -1,20 +1,22 @@
 """Project constants."""
-from re import escape
+from re import compile, escape
 
 specials = escape("!#$%&'*+-/=?^_`{|?.")
 
-EMAIL_REGEX = ''.join(
-    (
-        '^(?![',
-        specials,
-        '])(?!.*[',
-        specials,
-        ']{2})(?!.*[',
-        specials,
-        ']$)[A-Za-z0-9',
-        specials,
-        ']+(?<![',
-        specials,
-        '])@[A-Za-z0-9.-]+[.][A-Za-z]{2,4}$',
+EMAIL_REGEX = compile(  # noqa: WPS421
+    ''.join(
+        (
+            '^(?![',
+            specials,
+            '])(?!.*[',
+            specials,
+            ']{2})(?!.*[',
+            specials,
+            ']$)[A-Za-z0-9',
+            specials,
+            ']+(?<![',
+            specials,
+            '])@[A-Za-z0-9.-]+[.][A-Za-z]{2,4}$',
+        ),
     ),
 )
