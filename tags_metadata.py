4b0f6aeb0122255a9f3e2e880f406a7c9f832733
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-11 14:33:10 +0200

    dev created tags_metadata

---
+++ tags_metadata.py
@@ -0,0 +1,16 @@
+"""Project metadata."""
+
+metadata = [
+    {
+        'name': 'Authorization application',
+        'description': "Available endpoints from 'authorization' app",
+    },
+    {
+        'name': 'Posts application',
+        'description': "Available endpoints from 'posts' app",
+    },
+    {
+        'name': 'Users application',
+        'description': "Available endpoints from 'users' app",
+    },
+]
