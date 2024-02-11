7e499da6a12b9731f658134d7be380f837c056c9
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-11 16:42:03 +0200

    dev include likes_router into app in main

--- apps/main.py
+++ apps/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from apps.authorization.routers import authorization_router
 from apps.common.exceptions import BackendError
 from apps.common.exceptions_handlers import backend_error_handler
+from apps.likes.routers import likes_router
 from apps.posts.routers import posts_router
 from apps.user.routers import users_router
 from settings import Settings
@@ -26,3 +27,4 @@ app.add_exception_handler(BackendError, backend_error_handler)  # type: ignore
 app.include_router(users_router)
 app.include_router(authorization_router)
 app.include_router(posts_router)
+app.include_router(likes_router)
