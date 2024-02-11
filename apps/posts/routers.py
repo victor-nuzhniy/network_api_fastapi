9f87d6a39ccb09606095213d78f7e99299f013cd
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-11 13:06:59 +0200

    dev created posts routers

---
+++ apps/posts/routers.py
@@ -0,0 +1,43 @@
+"""Posts apps routers."""
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing_extensions import Annotated
+
+from apps.common.dependencies import get_async_session
+from apps.common.schemas import JSENDFailOutSchema, JSENDOutSchema
+from apps.common.user_dependencies import get_current_user
+from apps.posts.handlers import post_handlers
+from apps.posts.schemas import CreatePostIn, CreatePostOut
+from apps.user.models import User
+
+post_router = APIRouter()
+
+
+@post_router.post(
+    '/post/',
+    name='create_post',
+    response_model=JSENDOutSchema[CreatePostOut],
+    summary='Create post',
+    responses={
+        200: {'description': 'Successful create user response'},
+        422: {'model': JSENDFailOutSchema, 'description': 'ValidationError'},
+    },
+    tags=['Users application'],
+)
+async def create_post(
+    request: Request,
+    post: CreatePostIn,
+    user: Annotated[User, Depends(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+) -> dict:
+    """Create post router."""
+    created_post: CreatePostOut = await post_handlers.create_post(
+        request,
+        post,
+        user,
+        session,
+    )
+    return {
+        'data': created_post,
+        'message': 'Created post with id {id}'.format(id=created_post.id),
+    }
