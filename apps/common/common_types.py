f2aa703bdfe2ad67ff9a20eb3ef8e7a1f6ddbb3d
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-12 17:52:29 +0200

    dev added classmethod to LocalSchema in common_types

--- apps/common/common_types.py
+++ apps/common/common_types.py
@@ -1,14 +1,20 @@
 """Common project types."""
+from __future__ import annotations
+
 from pydantic import BaseModel
-from typing_extensions import TypeVar
+from typing_extensions import Any, Protocol, TypeVar
 
 from apps.common.db import Base
 
 ModelType = TypeVar('ModelType', bound=Base)
-SchemaType = TypeVar('SchemaType', bound=BaseModel)
+SchemaType = TypeVar('SchemaType', bound=BaseModel, covariant=True)
 
 
-class LocalSchema(BaseModel):
+class LocalSchema(Protocol[SchemaType]):
     """Class for type checking."""
 
     id: int
+
+    @classmethod
+    def model_validate(cls, *args: Any, **kwargs: Any) -> SchemaType:
+        """Validate model."""
