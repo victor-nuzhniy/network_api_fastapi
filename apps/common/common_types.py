06ac9b06acbaf389832ac3f86d2693b03492aa34
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-12 22:17:15 +0200

    dev deleted unused types in common_types.py

--- apps/common/common_types.py
+++ apps/common/common_types.py
@@ -2,19 +2,9 @@
 from __future__ import annotations
 
 from pydantic import BaseModel
-from typing_extensions import Any, Protocol, TypeVar
+from typing_extensions import TypeVar
 
 from apps.common.db import Base
 
 ModelType = TypeVar('ModelType', bound=Base)
 SchemaType = TypeVar('SchemaType', bound=BaseModel, covariant=True)
-
-
-class LocalSchema(Protocol[SchemaType]):
-    """Class for type checking."""
-
-    id: int
-
-    @classmethod
-    def model_validate(cls, *args: Any, **kwargs: Any) -> SchemaType:
-        """Validate model."""
