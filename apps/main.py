72f5133bf8ab412aeb97aea771eb5f870186a20d
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-14 19:28:25 +0200

    dev added exception_handlers to app

--- apps/main.py
+++ apps/main.py
@@ -1,10 +1,17 @@
 """Main FastAPI module."""
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import ValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from apps.authorization.routers import authorization_router
 from apps.common.exceptions import BackendError
-from apps.common.exceptions_handlers import backend_error_handler
+from apps.common.exceptions_handlers import (
+    backend_error_handler,
+    http_exception_handler,
+    validation_exception_handler,
+)
 from apps.likes.routers import likes_router
 from apps.posts.routers import posts_router
 from apps.user.routers import users_router
@@ -23,6 +30,19 @@ app.add_middleware(
 
 
 app.add_exception_handler(BackendError, backend_error_handler)  # type: ignore
+app.add_exception_handler(
+    StarletteHTTPException,
+    http_exception_handler,  # type: ignore
+)
+app.add_exception_handler(
+    RequestValidationError,
+    validation_exception_handler,  # type: ignore
+)
+app.add_exception_handler(
+    ValidationError,
+    validation_exception_handler,  # type: ignore
+)
+
 
 app.include_router(users_router)
 app.include_router(authorization_router)
