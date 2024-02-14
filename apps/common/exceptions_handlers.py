808e002cee613d3ffdfa6599e318d94020732741
victro-nuzhniy <nuzhniyva@gmail.com>
2024-02-14 19:24:33 +0200

    dev added exception_handlers

--- apps/common/exceptions_handlers.py
+++ apps/common/exceptions_handlers.py
@@ -1,7 +1,10 @@
 """App exceptions handlers."""
 from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse, Response
+from pydantic import ValidationError
 from sqlalchemy.exc import IntegrityError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from apps.common.enum import JSENDStatus
 from apps.common.exceptions import BackendError
@@ -11,6 +14,23 @@ from settings import Settings
 def backend_error_handler(request: Request, exc: BackendError) -> Response:
     """Return result from Back-end exception."""
     return JSONResponse(content=exc.dict(), status_code=exc.code)
+
+
+def http_exception_handler(
+    request: Request,
+    exc: StarletteHTTPException,
+) -> JSONResponse:
+    """Get the original 'detail', 'status_code' and 'headers'."""
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            'status': JSENDStatus.FAIL,
+            'data': exc.detail,
+            'message': 'Validation error.',
+            'code': exc.status_code,
+        },
+        headers=exc.headers,
+    )
 
 
 def integrity_error_handler(error: IntegrityError) -> None:
@@ -26,3 +46,30 @@ def integrity_error_handler(error: IntegrityError) -> None:
             code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             status=JSENDStatus.ERROR,
         )
+
+
+def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError | ValidationError,
+) -> JSONResponse:
+    """Get the original 'detail' list of errors."""
+    details = exc.errors()
+    modified_details = []
+    for error in details:
+        modified_details.append(
+            {
+                'location': error['loc'],
+                'message': error['msg'].capitalize(),
+                'type': error['type'],
+                'context': str(error.get('ctx', None)),
+            },
+        )
+    return JSONResponse(
+        content={
+            'status': JSENDStatus.FAIL,
+            'data': modified_details,
+            'message': 'Validation error.',
+            'code': status.HTTP_422_UNPROCESSABLE_ENTITY,
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
